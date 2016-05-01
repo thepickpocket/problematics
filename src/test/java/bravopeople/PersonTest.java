55f086fb961bda46026f97658892a3b803e9d14c
KaleabTessera <kaleabtessera@gmail.com>
2016-05-02 01:46:25 +0200

    Added initial tests.

--- src/test/java/bravopeople/PersonTest.java
+++ src/test/java/bravopeople/PersonTest.java
@@ -1,23 +1,58 @@
 package bravopeople;
 
+import static junit.framework.Assert.assertEquals;
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-public class PersonTest extends TestCase
+
+public class PersonTest
 {
-    public PersonTest(String testName){ super(testName); }
 
+    private Person test;
+    @Before
+    public void setUp(){
+        test = new Person("name", "surname", "id", "email");
+    }
     /**
      *
      * TODO: Add proper tests once code is in place.
      * Tests
      *  - Contructor params are same as gets
      */
+    @Test
     public void testPerson()
     {
-        Person test = new Person("name", "surname", "id", "email");
+        
         assertEquals("name", test.getName());
         assertEquals("surname", test.getSurname());
         assertEquals("id", test.getID());
         assertEquals("email", test.getEmail());
+    }
+    
+    @Test
+    public void testsetUserRights(){
+        //Default
+        test.setUserRights("Random");
+        assertEquals("RESEARCHER", test.getUserRights());
+        
+        test.setUserRights("ADMIN");
+        assertEquals("ADMIN", test.getUserRights());
+        
+        test.setUserRights("RESEARCHGROUPLEADER");
+        assertEquals("RESEARCHGROUPLEADER", test.getUserRights());
+        
+        test.setUserRights("RESEARCHER");
+        assertEquals("RESEARCHER", test.getUserRights());
+    }
+    
+    @Test
+    public void testsetOrganisation(){
+        Organisation orgMock = mock(Organisation.class);
+        when(orgMock.getName()).thenReturn("Org1");
+        test.setOrganisation(orgMock);
+        assertEquals("Org1", test.getOrganisation().getName());
     }
 }
