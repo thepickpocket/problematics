0b79b569bf3841bf7a2175479636478f1d04701c
Jason Evans <evans.richard.jason@gmail.com>
2016-05-04 08:37:29 +0200

    Tests for module: Group Status: Complete

--- src/test/java/bravopeople/GroupTest.java
+++ src/test/java/bravopeople/GroupTest.java
@@ -4,30 +4,66 @@ import org.junit.Test;
 import org.junit.BeforeClass;
 import java.util.ArrayList;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
-import static org.junit.Assert.assertEquals;
 
 public class GroupTest
 {
     public static Group testGroup;
-    //public static ArrayList<PersonEntity> subGroups;
+    public static int numMockedPeople = 5;
+    public static ArrayList<PersonEntity> mockedPeople = new ArrayList<>();
 
     @BeforeClass
     public static void setup(){
-        testGroup = new Group("Testing Group", new ArrayList<PersonEntity>());
+        for (int i = 0; i < numMockedPeople; i++)
+            mockedPeople.add(mock(PersonEntity.class));
+        testGroup = new Group("Testing Group", mockedPeople);
     }
 
     @Test
-    public void testGroup()
+    public void test_Group_Creation()
     {
         assertTrue(testGroup.getName().equals("Testing Group"));
+        assertTrue(testGroup.getEntities().size() == numMockedPeople);
     }
+
 
     @Test
     public void test_Group_Null_String(){
+        assertNotNull("The name was never initialized on creation.", testGroup.getName());
         testGroup.setName(null);
-        assertNotNull("Cannot assign a null value to a name.", testGroup.getName());
+        assertNotNull("Should not be able to assign a null string as the group name.", testGroup.getName());
+    }
+
+    @Test
+    public void test_Group_NotNull_String(){
+        testGroup.setName("Testing Group name change");
+        assertTrue(testGroup.getName().equals("Testing Group name change"));
+    }
+
+    @Test
+    public void test_Adding_Person_Entity(){
+        testGroup.addEntity(mock(PersonEntity.class));
+        assertTrue(testGroup.getEntities().size() == (numMockedPeople+1));
+    }
+
+    @Test
+    public void test_Get_Active_Status_Default(){
+        assertTrue(testGroup.isActive());
+    }
+
+    @Test
+    public void test_Change_Active_Status_Inactive(){
+        testGroup.deactivate();
+        assertFalse("Group was not successfully deactivated.", testGroup.isActive());
+    }
+
+    @Test
+    public void test_Change_Active_Status_Active(){
+        testGroup.deactivate();
+        testGroup.activate();
+        assertTrue("Group was not successfully re-activated.", testGroup.isActive());
     }
 }
