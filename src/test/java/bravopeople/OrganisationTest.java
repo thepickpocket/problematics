73417c5ce7db588ee5f994cd0a58249dcb4bee9f
Thinus naude <thinusn@gmail.com>
2016-05-10 17:32:16 +0200

    Updated Organisation tests

--- src/test/java/bravopeople/OrganisationTest.java
+++ src/test/java/bravopeople/OrganisationTest.java
@@ -24,37 +24,48 @@ public class OrganisationTest
     }
 
     /**
-     * Tests
-     *  - Contructor name is same as getName
+     * Test Case O1: Tests constructor name is same as getName
      */
     @Test
-    public void testOrganisation()
+    public void test_Organisation_Default_Constructor()
     {
         Organisation test = new Organisation();
         assertEquals("Organisation", test.getName());
     }
 
     /**
-     * Tests
-     *  - Contructor name is same as getName
+     * Test Case  O2: Tests the constructor name is same as getName
      */
     @Test
-    public void testOrganisationTwo() {
+    public void test_Organisation_Name_Only_Constructor() {
         Organisation test = new Organisation("Only a Name");
         assertEquals("Only a Name", test.getName());
     }
 
     /**
-     * Tests if the organisation was created
+     * Test Case O3: Tests if the organisation was created
      */
     @Test
-    public void test_Organisation_Creation() {
+    public void test_Organisation_Creattion() {
         assertNotNull("Organisation Created", testOrganisation);
         assertNotNull("The name was never initialized on creation.", testOrganisation.getName());
     }
 
+
     /**
-     * Tests if the organisation getter and setter methods exist for the ID variable
+     * Test Case O4: Tests if the organisation getter and setter methods exist for the Name variable
+     */
+    @Test
+    public void test_Organisation_Name_Methods_Exist() {
+        String s = "New Name";
+        testOrganisation.setName(s);
+        assertTrue("ID value setter did not work", testOrganisation.getName().equals(s));
+        assertTrue("ID value getter did not work", testOrganisation.getName().equals(s));
+    }
+
+
+    /**
+     * Test Case O5: Tests if the organisation getter and setter methods exist for the ID variable
      */
     @Test
     public void test_Organisation_ID_Methods_Exist() {
@@ -65,81 +76,7 @@ public class OrganisationTest
     }
 
     /**
-     * Tests if the organisation getter and setter methods exist for the ID variable
-     */
-    @Test
-    public void test_Organisation_Name_Methods_Exist() {
-        String s = "New Name";
-        testOrganisation.setName(s);
-        assertTrue("ID value setter did not work", testOrganisation.getName().equals(s));
-        assertTrue("ID value getter did not work", testOrganisation.getName().equals(s));
-
-    }
-
-    /**
-     * Tests if the name of the organisation cant be set to an null string
-     */
-    @Test(expected = IllegalArgumentException.class)
-    public void test_Organisation_Null_String() {
-        try {
-            testOrganisation.setName(null);
-            fail("Illegal Argument Exception should have been thrown - Name cant be null");
-        } catch (Exception ex) {
-            assertTrue("Illegal Argument Exception should have been thrown - Name cant be null", ex instanceof NullPointerException);
-        }
-    }
-
-    /**
-     * Tests if the name of the organisation cant be set to an empty string
-     */
-    @Test
-    public void test_Organisation_Empty_String() {
-        try {
-            testOrganisation.setName("");
-            fail("Illegal Argument Exception should have been thrown - Name cant be empty");
-        } catch (Exception ex) {
-            assertTrue("Illegal Argument Exception should have been thrown - Name cant be empty", ex instanceof IllegalArgumentException);
-        }
-    }
-
-    /**
-     * Tests if the name of the organisation cant be set to only a number
-     */
-    @Test
-    public void test_Organisation_Name_Not_Number() {
-        try {
-            testOrganisation.setName("1");
-            fail("Illegal Argument Exception should have been thrown - Name cant be a number");
-        } catch (Exception ex) {
-            assertTrue("Illegal Argument Exception should have been thrown - Name cant be a number", ex instanceof IllegalArgumentException);
-        }
-    }
-
-    /**
-     * Tests if the name of the organisation can be changed
-     */
-    @Test
-    public void test_Organisation_NotNull_String() {
-        testOrganisation.setName("Testing Organisation name change Before");
-        testOrganisation.setName("Testing Organisation name change After");
-        assertTrue("Organisation name change did not work", testOrganisation.getName().equals("Testing Organisation name change After"));
-    }
-
-    /**
-     * Tests if the name of the organisation is not to short
-     */
-    @Test
-    public void test_Organisation_MinLenght_String() {
-        try {
-            testOrganisation.setName("a");
-            fail("Illegal Argument Exception should have been thrown - Name String cant just be one letter");
-        } catch (Exception ex) {
-            assertTrue("Illegal Argument Exception should have been thrown - Name String cant just be one letter", ex instanceof IllegalArgumentException);
-        }
-    }
-
-    /**
-     * Tests if the id of the organisation cant be set to an null string
+     * Test Case O6: Tests if the id of the organisation cant be set to an null string
      */
     @Test
     public void test_Organisation_ID_Null_String() {
@@ -152,7 +89,7 @@ public class OrganisationTest
     }
 
     /**
-     * Tests if the ID of the organisation cant be set to an empty string
+     * Test Case O7: Tests if the ID of the organisation cant be set to an empty string
      */
     @Test(expected = IllegalArgumentException.class)
     public void test_Organisation_ID_Empty_String() {
@@ -165,13 +102,78 @@ public class OrganisationTest
     }
 
     /**
-     * Tests if the ID of the organisation can be changed
+     * Test Case O8: Tests if the ID of the organisation can be retrieved
      */
     @Test
-    public void test_Organisation_ID_NotNull_String() {
+    public void test_Organisation_ID_Change() {
 //        testOrganisation.setID("Testing Organisation ID change Before");
 //        testOrganisation.setID("Testing Organisation ID change After");
 //        assertTrue("Organisation ID change did not work", testOrganisation.getID().equals("Testing Organisation ID change After"));
         fail("Organisation ID change did not work - organisation.getID() method does not exist");
     }
+
+    /**
+     * Test Case  O9: Tests if the name of the organisation cant be set to an null string
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void test_Organisation_Name_Null_String() {
+        try {
+            testOrganisation.setName(null);
+            fail("Illegal Argument Exception should have been thrown - Name cant be null");
+        } catch (Exception ex) {
+            assertTrue("Illegal Argument Exception should have been thrown - Name cant be null", ex instanceof NullPointerException);
+        }
+    }
+
+    /**
+     * Test Case O10: Tests if the name of the organisation cant be set to an empty string
+     */
+    @Test
+    public void test_Organisation_Name_Empty_String() {
+        try {
+            testOrganisation.setName("");
+            fail("Illegal Argument Exception should have been thrown - Name cant be empty");
+        } catch (Exception ex) {
+            assertTrue("Illegal Argument Exception should have been thrown - Name cant be empty", ex instanceof IllegalArgumentException);
+        }
+    }
+
+    /**
+     * Test Case O11: Tests if the name of the organisation cant be set to only a number
+     */
+    @Test
+    public void test_Organisation_Name_Not_Number() {
+        try {
+            testOrganisation.setName("1");
+            fail("Illegal Argument Exception should have been thrown - Name cant be a number");
+        } catch (Exception ex) {
+            assertTrue("Illegal Argument Exception should have been thrown - Name cant be a number", ex instanceof IllegalArgumentException);
+        }
+    }
+
+    /**
+     * Test Case O12: Tests if the name of the organisation is not to short
+     */
+    @Test
+    public void test_Organisation_MinLenght_String() {
+        try {
+            testOrganisation.setName("a");
+            fail("Illegal Argument Exception should have been thrown - Name String cant just be one letter");
+        } catch (Exception ex) {
+            assertTrue("Illegal Argument Exception should have been thrown - Name String cant just be one letter", ex instanceof IllegalArgumentException);
+        }
+    }
+
+    /**
+     * Test Case O13: Tests if the name of the organisation can be changed
+     */
+    @Test
+    public void test_Organisation_Name_Change() {
+        testOrganisation.setName("Testing Organisation name change Before");
+        testOrganisation.setName("Testing Organisation name change After");
+        assertTrue("Organisation name change did not work", testOrganisation.getName().equals("Testing Organisation name change After"));
+    }
+
+
+
 }
